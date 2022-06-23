90e7e640c930041a56fb39ebb563a56d78811d72
gracechiu <rick.grace.1014@gmail.com>
2022-06-23 15:58:14 +0800

    表單建立使用POST

---
+++ welcome.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- <h1>Welcome to this site</h1>
+    <h3>this is a test page</h3> -->
+
+    Welcome <?php echo $_POST['name'];?><br>
+    Your email address is: <?php echo $_POST['email']?>
+</body>
+</html>
