37406c7a2825491e4eaec8943290147a9aed0542
gracechiu <rick.grace.1014@gmail.com>
2022-06-24 10:20:04 +0800

    會員註冊資訊頁面

---
+++ user_register.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .error{color: red;}
+    </style>
+</head>
+<body>
+    <?php
+    $nameErr = $emailErr = $genderErr = $numberErr = "";
+    $name = $email = $gender = $comment = $number = "";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST['name'])) {
+            $nameErr = "此為必填欄位";
+        }else{
+            $name = test_input($_POST["name"]); 
+        }
+        if (empty($_POST["email"])) {
+            $emailErr = "此為必填欄位";
+        }else{
+            $email = test_input($_POST["email"]);
+
+        }
+        if (empty($_POST["number"])) {
+            $numberErr = "此為必填欄位";
+        }else{
+            $number = test_input($_POST["number"]);
+
+        }
+        if (empty($_POST["gender"])) {
+            $genderErr = "此為必填欄位";
+        }else{
+            $gender = test_input($_POST["gender"]);
+        }
+        
+        $comment = test_input($_POST["comment"]);
+       
+
+    }
+    function test_input($data){
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+    ?>
+
+    <h2>會員註冊資訊</h2>
+    <p><span class="error">*請留意紅字為必填欄位</span></p>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?>">
+     中文姓名 Name: <input type="text" name="name">
+     <span class="error">*<?php echo $nameErr?></span>
+     <br><br>
+     電子郵件 Email: <input type="text" name="email">
+     <span class="error">* <?php echo $emailErr?></span>
+     <br><br>
+     聯繫資訊 Phone number: <input type="text" name="number">
+     <span class="error">* <?php echo $numberErr?></span>
+     <br><br>
+     其他說明事項 Comment: <textarea name="comment" cols="40" rows="5"></textarea>
+     <br><br>
+     性別 Gender:
+     <input type="radio" name="gender" value="female">女性 Female
+     <input type="radio" name="gender" value="male"> 男性 male
+     <input type="radio" name="gender" value="other"> 其他 other
+     <span class="error">* <?php echo $genderErr?></span>
+     <br><br>
+     <input type="submit" name="Submit" value=" 提交 Submit">
+
+</form>
+
+     <?php
+     echo "<h2>感謝您的填寫</h2>";
+     echo $name;
+     echo "<br>";
+     echo $email;
+     echo "<br>";
+     echo $number;
+     echo "<br>";
+     echo $comment;
+     echo "<br>";
+     echo $gender;
+
+
+     ?>
+</body>
+</html>
