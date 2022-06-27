7f876365408dd56c4937c7feda8404538a0769c1
gracechiu <rick.grace.1014@gmail.com>
2022-06-27 10:07:04 +0800

    學習cookie的建立刪除

---
+++ L2/a02.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<?php
+    $cookie_name = "user";
+    $cookie_value = "Alex Porter";
+    setcookie($cookie_name, $cookie_value, time() - 3600);//86400 = 1 day
+
+    ?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   
+    <?php
+//     if (!isset($_COOKIE[$cookie_name])) {
+//         echo "Cookie named '" . $cookie_name . "' is not set!";
+//     } else {
+//            echo "Cookie '" . $cookie_name . "' is set!<br>";
+//            echo "Value is: " . $_COOKIE[$cookie_name];
+    
+// } 
+
+     echo "Cookie 'user' is deleted";
+    ?>
+
+    <p><strong>Note:</strong> You might have to reload the page to see the value of the cookie.</p>
+</body>
+</html>
