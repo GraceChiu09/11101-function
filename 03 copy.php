676324131b6682dc9ae73743cb91a94fbce07f1a
gracechiu <rick.grace.1014@gmail.com>
2022-06-27 15:41:44 +0800

    PDO建立資料表

--- 03 copy.php
+++ 03 copy.php
@@ -11,51 +11,30 @@
     $servername = "localhost";
     $username = "username";
     $password = "password";
-    $dbname = "myDB";
+    $dbname = "myDBPDO";
 
     // create
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-
-    }
-
-    // sql create table
-    $sql = "CREATE TABLE MyTables(
+   try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "CREATE TABLE Mytest02 (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     firstname VARCHAR(30) NOT NULL,
     lastname VARCHAR(30) NOT NULL,
     email VARCHAR(50),
     reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-    )";
+  )";
+    $conn->exec($sql);
+    echo "Table Mytest02 created successfully";
+    
+    
+    
 
-    if ($conn->query($sql) === TRUE) {
-        echo "Table MyGuests created successfully";
-    }else{
-        echo "Error creating table: " . $conn->error;
-    
-    }
-    // close
+   } catch (PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
+   }
 
-    $conn->close();
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+   $conn = null;
     
     ?>
 </body>
