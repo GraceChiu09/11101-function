de68b5d6d7f629476ef49608d0612d80ed4f04f5
gracechiu <rick.grace.1014@gmail.com>
2022-06-29 16:44:22 +0800

    陣列練習

---
+++ 03.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $one_array = ["1" => "Alice", "2" => "Jack", "3" => "Lucy"];
+    // echo $one_array["1"] . "<br>";
+    // echo $one_array["2"] . "<br>";
+    // echo $one_array["3"] . "<br>";
+
+    // var_dump($one_array);
+
+    print_r($one_array);
+       
+    ?>
+</body>
+</html>
